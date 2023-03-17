515da173e86dfa7775dd30d31a1241dafebd9177
Victor <victor.chumba@student.moringaschool.com>
2023-03-17 11:12:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+let
 function scuberGreetingForFeet(){
-  // Write your code here!
+  if (ride<=400 feet){
+    return" This one is on me!"
+  }else 
 }
 
 function ternaryCheckCity(){
+  else if
   // Write your code here!
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  if(tipGenerous){
+  return"Thank you so much" 
+  }else if(tipNotAsGenerous){
+    return"Thankyou"
+  }else(anythingelse){
+    return"Bye"
+  }
 }
